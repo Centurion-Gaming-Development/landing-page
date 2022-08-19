d4f0d8b5125157ad5e54bdf343d2171756dd5f3c
nick nick <nickcboboaca@gmail.com>
2022-08-19 12:50:41 -0700

    :sparkles: Init create react app, tailwindcss, eslint

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Home = () => {
+  console.log("home");
+  return (
+    <div className="text-white p-5">
+      <p className="text-4xl">Hello world</p>
+    </div>
+  );
+};
+
+export default Home;
